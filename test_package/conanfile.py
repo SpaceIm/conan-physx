aa3999abc08706a052c0769f2f67edbf2c870d20
SpaceIm <30052553+SpaceIm@users.noreply.github.com>
2021-04-02 14:37:32 +0200

    sync with CCI

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,13 +1,14 @@
+from conans import ConanFile, CMake, tools
 import os
 
-from conans import ConanFile, CMake, tools
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build(self):
         cmake = CMake(self)
+        cmake.definitions["TEST_SHARED_LIBRARY"] = True if "fPIC" not in self.options["physx"].fields else self.options["physx"].fPIC
         cmake.configure()
         cmake.build()
 
